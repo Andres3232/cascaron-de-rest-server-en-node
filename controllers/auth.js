3605403b5d2dd25412b57b8b2fc75e33896e9d13
Andres3232 <andresmurgo@gmail.com>
2021-06-30 21:10:52 -0300

    Googel sigin added

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,55 +1,103 @@
-const bcryptjs = require('bcryptjs');
+const { response } = require('express');
+const bcryptjs = require('bcryptjs')
 
-const { generarJWT } = require('../helpers/generarJWT');
 const Usuario = require('../models/usuario');
 
+const { generarJWT } = require('../helpers/generarJWT');
+const { googleVerify } = require('../helpers/google-verify');
 
 
-const login = async(req, res  )  => {
+const login = async(req, res = response) => {
 
     const { correo, password } = req.body;
 
     try {
-
-        //VERIFICAR SI EL EMIAL EXISTE
-        const usuario = await Usuario.findOne({correo});
-        if(!usuario){
+      
+        // Verificar si el email existe
+        const usuario = await Usuario.findOne({ correo });
+        if ( !usuario ) {
             return res.status(400).json({
-                msg:'Usuario / Password no son correctos - correo'
+                msg: 'Usuario / Password no son correctos - correo'
             });
         }
 
-        //SI EL USUARIO ESTA ACTIVO
-        if(!usuario.estado){
+        // SI el usuario está activo
+        if ( !usuario.estado ) {
             return res.status(400).json({
-                msg:'Usuario / Password no son correctos - estado: false'
+                msg: 'Usuario / Password no son correctos - estado: false'
             });
         }
 
-        //VERIFICAR EL PASSWORD
+        // Verificar la contraseña
         const validPassword = bcryptjs.compareSync( password, usuario.password );
-        if(!validPassword){
+        if ( !validPassword ) {
             return res.status(400).json({
-                msg:'Usuario / Password no son correctos - password'
+                msg: 'Usuario / Password no son correctos - password'
             });
         }
 
+        // Generar el JWT
+        const token = await generarJWT( usuario.id );
 
-        //GENERAR EL JWT
-        const token = await generarJWT ( usuario.id );
-        
-        res.json ({
+        res.json({
             usuario,
             token
-    
         })
-        
+
     } catch (error) {
         console.log(error)
-        return res.status(500).json({
-            msg: 'Hable con el admin'
+        res.status(500).json({
+            msg: 'Hable con el administrador'
+        });
+    }   
+
+}
+
+
+const googleSignin = async(req, res = response) => {
+
+    const { id_token } = req.body;
+    
+    try {
+        const { correo, nombre, img } = await googleVerify( id_token );
+
+        let usuario = await Usuario.findOne({ correo });
+
+        if ( !usuario ) {
+            // Tengo que crearlo
+            const data = {
+                nombre,
+                correo,
+                password: ':P',
+                img,
+                google: true
+            };
+
+            usuario = new Usuario( data );
+            await usuario.save();
+        }
+
+        // Si el usuario en DB
+        if ( !usuario.estado ) {
+            return res.status(401).json({
+                msg: 'Hable con el administrador, usuario bloqueado'
+            });
+        }
+
+        // Generar el JWT
+        const token = await generarJWT( usuario.id );
+        
+        res.json({
+            usuario,
+            token
         });
         
+    } catch (error) {
+
+        res.status(400).json({
+            msg: 'Token de Google no es válido'
+        })
+
     }
 
 
@@ -57,6 +105,8 @@ const login = async(req, res  )  => {
 }
 
 
+
 module.exports = {
-    login
+    login,
+    googleSignin
 }
--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -72,11 +72,8 @@ const { Promise } = require('mongoose');
   const usuariosDelete = async(req, res = response) => {
 
     const { id } = req.params;
-    
-
     //Fisicamente lo borramos
     //const usuario = await Usuario.findByIdAndDelete( id );
-
     const usuario = await Usuario.findByIdAndUpdate( id, { estado: false} );
     
 
